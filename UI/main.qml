dd2caf7072c0fae066b6ed2460635b6eade44b66
RohidipRider <>
2021-12-07 04:15:16 +0000

    Initial commit

---
+++ UI/main.qml
@@ -0,0 +1,28 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+
+ApplicationWindow {
+    visible: true
+    width: 1920
+    height: 1080
+    title: "HelloApp"
+    flags: Qt.FramelessWindowHint | Qt.Window
+    Rectangle {
+        anchors.fill: parent
+        Image {
+            sourceSize.width: parent.width
+            sourceSize.height: parent.height
+            source: "./blue screen.jpg"
+            fillMode: Image.PreserveAspectCrop
+        }
+        Rectangle {
+            anchors.fill: parent
+            color: "transparent"
+            Text {
+                text: ""
+                font.pixelSize: 24
+                color: "white"
+            }
+        }
+    }
+}
---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+def blue():
+	app = QGuiApplication(sys.argv)
+	engine = QQmlApplicationEngine()
+	engine.quit.connect(app.quit)
+	engine.load('./UI/main.qml')
+	sys.exit(app.exec())
+
+misspell = input("Please spell /misˈspel/, the words are phonetic so you cant cheat\n");
+
+if misspell == "misspell":
+	pharaoh = input("Yay you did it, lets move on to a harder one, please spell /ˈferō/\n");
+  if pharaoh == "pharaoh":
+		print ("yay")
+	
+  else:
+    blue()
+
+else:
+  blue()
+
+def blue();
+	app = QGuiApplication(sys.argv)
+	engine = QQmlApplicationEngine()
+	engine.quit.connect(app.quit)
+	engine.load('./UI/main.qml')
+	sys.exit(app.exec())
